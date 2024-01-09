2e2ef75184709c418333d1e01f56f3f65c019d21
Kim Robinson <kimmykokonut@gmail.com>
2024-01-09 13:56:04 -0800

    fix bug and search show by city/state fully functional

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -6,7 +6,7 @@ function Browse() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [eventsNearby, setEventsNearby] = useState([]);
-  const [selectedState, setSelectedState] = useState('US-AL'); //want 'US-NY'
+  const [selectedState, setSelectedState] = useState('US-AL');
   const [selectCity, setSelectCity] = useState('Portland');
 
   useEffect(() => { //this is for Portland OR API call
@@ -37,13 +37,9 @@ function Browse() {
 
   async function handleClick(e) {
     e.preventDefault();
-    console.log(selectCity, selectedState);
     const jamID = await getCityId(selectCity, selectedState);
-    console.log(jamID);
-    const finalResult = getShowsById(jamID);
+    const finalResult = await getShowsById(jamID);
     setEventsNearby(finalResult);
-    console.log(eventsNearby);
-    //want this to go in return
   }
 
   function handleChange(e) {
@@ -60,56 +56,61 @@ function Browse() {
   } else if (!isLoaded) {
     return <h1>...Loading...</h1>;
   } else {
-    return (
-      <>
-        <h1>Let's go check some sounds!</h1>
-        <form>
-          <input
-            name="city"
-            placeholder="my city..."
-            type="text"
-            onChange={handleChange}>
-          </input>
+    if (eventsNearby) {
+      return (
+        <>
+          <h1>Let's go check some sounds!</h1>
+          <form>
+            <input
+              name="city"
+              placeholder="my city..."
+              type="text"
+              onChange={handleChange}>
+            </input>
+            <br />
+            <label htmlFor="state">State: </label>
+            <select id="state" name="state" onChange={handleChange}>
+              {Object.keys(geoStateIso).map(key => {
+                return (
+                  <option name="state" value={key} key={key}>{geoStateIso[key]}
+                  </option>
+                );
+              })};
+            </select>
+            <br />
+            <button onClick={handleClick}>show me shows near me</button>
+          </form>
+          <hr />
+          <h3>Who's coming to {selectCity}?</h3>
+          <ul>
+            {eventsNearby && eventsNearby.map((show, index) =>
+              <li key={index}>
+                <h3>{show.name}</h3>
+                <h4>{
+                  new Date(show.startDate).toLocaleString('en-US', {
+                    year: 'numeric',
+                    month: 'long',
+                    day: 'numeric',
+                    hour: '2-digit',
+                    minute: '2-digit',
+                    hour12: true
+                  })
+                }</h4>
+                <h4>{show.location.name}</h4>
+                <p>{show.location.address.streetAddress}</p>
+                {show.offers && show.offers[0] && show.offers[0].url ? <a href={show.offers[0].url}>link to venue</a> : null}
+                <br />
+                <button>Follow</button>
+                <hr />
+              </li>
+            )}
+          </ul>
           <br />
-          <label htmlFor="state">State: </label>
-          <select id="state" name="state" onChange={handleChange}>
-            {Object.keys(geoStateIso).map(key => {
-              return (
-                <option name="state" value={key} key={key}>{geoStateIso[key]}
-                </option>
-              );
-            })};
-          </select>
-          <br />
-          <button onClick={handleClick}>show me shows near me</button>
-        </form>
-        <hr />
-        <h3>Who's coming to {selectCity}?</h3>
-        <ul>
-          {eventsNearby && eventsNearby.map((show, index) =>
-            <li key={index}>
-              <h3>{show.name}</h3>
-              <h4>{
-                new Date(show.startDate).toLocaleString('en-US', {
-                  year: 'numeric',
-                  month: 'long',
-                  day: 'numeric',
-                  hour: '2-digit',
-                  minute: '2-digit',
-                  hour12: true
-                })
-              }</h4>
-              <h4>{show.location.name}</h4>
-              <p>{show.location.address.streetAddress}</p>
-              {show.offers && show.offers[0] && show.offers[0].url ? <a href={show.offers[0].url}>link to venue</a> : null}
-              <br />
-              <button>wanna go - add to my dashboard</button>
-              <hr />
-            </li>
-          )}
-        </ul>
-      </>
-    );
+          <button>Navigate to my dashboard</button>
+        </>
+      )
+    };
   }
 }
+
 export default Browse;
