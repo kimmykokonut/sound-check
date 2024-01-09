a21b657b2761091fbc715a4305549a6908436c25
Kim Robinson <55254544+kimmykokonut@users.noreply.github.com>
2024-01-09 15:44:52 -0800

    update Browse component  (#17)
    * add signout nav button and display signed in user email
    
    * begin pulling user data to display user show by db location, with default as portland

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -2,6 +2,9 @@ import { useState, useEffect } from "react";
 import { geoStateIso } from "../city-state-data";
 import { getCityId, getShowsById } from "../fetchData";
 import { useNavigate } from "react-router-dom";
+import { auth, db } from '../firebase';
+import { doc, getDoc } from 'firebase/firestore';
+
 
 function Browse() {
   const navigate = useNavigate();
@@ -10,30 +13,86 @@ function Browse() {
   const [eventsNearby, setEventsNearby] = useState([]);
   const [selectedState, setSelectedState] = useState('US-AL');
   const [selectCity, setSelectCity] = useState('Portland');
+  const [displayName, setDisplayName] = useState('');
 
-  useEffect(() => { //this is for Portland OR API call
-    fetch(`https://www.jambase.com/jb-api/v1/events?perPage=5&geoCityId=jambase%3A4247192&apikey=${import.meta.env.VITE_REACT_APP_JAMBASE}`, {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-      },
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`${response.status}: ${response.statusText}`);
-        } else {
-          return response.json()
-        }
-      })
-      .then((jsonifiedResponse) => {
-        setEventsNearby(jsonifiedResponse.events)
-        setIsLoaded(true)
-      })
-      .catch((error) => {
-        setError(error)
-        setIsLoaded(true)
-      });
-  }, [])
+  useEffect(() => {
+    const checkCurrentUser = async () => {
+      try {
+        auth.onAuthStateChanged((user) => {
+          if (user) {
+            setDisplayName(user.email || '');
+          }
+        });
+      } catch (error) {
+        setError(error.message);
+        //setIsLoaded(true);
+      }
+    };
+
+    checkCurrentUser();
+  }, []);
+
+  //this currently defaults to Pdx shows until we can get user db input state as 'US-CT' format
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        auth.onAuthStateChanged(async (user) => {
+          if (user) {
+            setDisplayName(user.email || '');
+            const userId = user.uid;
+            const userRef = doc(db, 'users', userId);
+            const userSnapshot = await getDoc(userRef);
+            console.log(userSnapshot);
+
+            if (userSnapshot.exists()) {
+              const userData = userSnapshot.data();
+              console.log(userData.city);
+              //call api call by city/state here?
+              //add to city once state is set: userData.city ||
+              const city = 'Portland'
+              setSelectCity(city);
+              const state = userData.state || 'US-OR';
+              setSelectedState(state);
+              const jamID = await getCityId(city, state);
+              const finalResult = await getShowsById(jamID);
+              setEventsNearby(finalResult);
+            } else {
+              console.log("User not found!");
+            }
+          }
+          setIsLoaded(true);
+        });
+      } catch (error) {
+        setError(error.message);
+        setIsLoaded(true);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // useEffect(() => { //this is for Portland OR API call
+  //   fetch(`https://www.jambase.com/jb-api/v1/events?perPage=5&geoCityId=jambase%3A4247192&apikey=${import.meta.env.VITE_REACT_APP_JAMBASE}`, {
+  //     method: 'GET',
+  //     headers: {
+  //       'Accept': 'application/json',
+  //     },
+  //   })
+  //     .then(response => {
+  //       if (!response.ok) {
+  //         throw new Error(`${response.status}: ${response.statusText}`);
+  //       } else {
+  //         return response.json()
+  //       }
+  //     })
+  //     .then((jsonifiedResponse) => {
+  //       setEventsNearby(jsonifiedResponse.events)
+  //       setIsLoaded(true)
+  //     })
+  //     .catch((error) => {
+  //       setError(error)
+  //       setIsLoaded(true)
+  //     });
+  // }, [])
 
   async function handleClick(e) {
     e.preventDefault();
@@ -59,10 +118,12 @@ function Browse() {
       return (
         <>
           <h1>Let's go check some sounds!</h1>
+          <p>signed in: {displayName}</p>
+          <hr />
           <form>
             <input
               name="city"
-              placeholder="my city..."
+              placeholder="city name..."
               type="text"
               onChange={handleChange}>
             </input>
@@ -77,7 +138,7 @@ function Browse() {
               })};
             </select>
             <br />
-            <button onClick={handleClick}>show me shows near me</button>
+            <button onClick={handleClick}>show me shows</button>
           </form>
           <hr />
           <h3>Who's coming to {selectCity}?</h3>
@@ -105,7 +166,9 @@ function Browse() {
             )}
           </ul>
           <br />
-          <button onClick={() => navigate('/userDashboard')}>Navigate to my dashboard</button>
+          <button onClick={() => navigate('/userDashboard')}>go to my dashboard</button>
+          <hr />
+          <button onClick={() => navigate('/')}>Sign Out</button>
         </>
       )
     };
