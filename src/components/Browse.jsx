21d63c47e98617d1a04bcf39594c0d6174444584
Kim Robinson <kimmykokonut@gmail.com>
2024-01-09 12:11:41 -0800

    wip. bug in display search results

--- src/components/Browse.jsx
+++ src/components/Browse.jsx
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
+import { geoStateIso } from "../city-state-data";
+import { getCityId, getShowsById } from "../fetchData";
 
 function Browse() {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [eventsNearby, setEventsNearby] = useState([]);
+  const [selectedState, setSelectedState] = useState('US-AL'); //want 'US-NY'
+  const [selectCity, setSelectCity] = useState('Portland');
 
-  useEffect(() => {
-    fetch(`https://www.jambase.com/jb-api/v1/events?perPage=20&geoCityId=jambase%3A4247192&apikey=${import.meta.env.VITE_REACT_APP_JAMBASE}`, {
+  useEffect(() => { //this is for Portland OR API call
+    fetch(`https://www.jambase.com/jb-api/v1/events?perPage=5&geoCityId=jambase%3A4247192&apikey=${import.meta.env.VITE_REACT_APP_JAMBASE}`, {
       method: 'GET',
       headers: {
         'Accept': 'application/json',
@@ -22,7 +26,7 @@ function Browse() {
       .then((jsonifiedResponse) => {
         setEventsNearby(jsonifiedResponse.events)
         setIsLoaded(true)
-        console.log(eventsNearby);
+        console.log(jsonifiedResponse.events);
       })
       .catch((error) => {
         console.error('Error:', error);
@@ -31,9 +35,25 @@ function Browse() {
       });
   }, [])
 
-  // function handleForm(){
-// have api call connected to either user input city or user aut set city....
-  // }
+  async function handleClick(e) {
+    e.preventDefault();
+    console.log(selectCity, selectedState);
+    const jamID = await getCityId(selectCity, selectedState);
+    console.log(jamID);
+    const finalResult = getShowsById(jamID);
+    setEventsNearby(finalResult);
+    console.log(eventsNearby);
+    //want this to go in return
+  }
+
+  function handleChange(e) {
+    if (e.target.name === 'state') {
+      console.log(e.target.name);
+      setSelectedState(e.target.value);
+    } else if (e.target.name === 'city') {
+      setSelectCity(e.target.value);
+    }
+  }
 
   if (error) {
     return <h1>Error: {error}</h1>;
@@ -45,12 +65,26 @@ function Browse() {
         <h1>Let's go check some sounds!</h1>
         <form>
           <input
-            placeholder="my location..."
-            type="text"></input>
-          <button type="submit">show me shows near me</button>
+            name="city"
+            placeholder="my city..."
+            type="text"
+            onChange={handleChange}>
+          </input>
+          <br />
+          <label htmlFor="state">State: </label>
+          <select id="state" name="state" onChange={handleChange}>
+            {Object.keys(geoStateIso).map(key => {
+              return (
+                <option name="state" value={key} key={key}>{geoStateIso[key]}
+                </option>
+              );
+            })};
+          </select>
+          <br />
+          <button onClick={handleClick}>show me shows near me</button>
         </form>
         <hr />
-        <h3>Who's coming to Portland?</h3>
+        <h3>Who's coming to {selectCity}?</h3>
         <ul>
           {eventsNearby && eventsNearby.map((show, index) =>
             <li key={index}>
@@ -67,7 +101,9 @@ function Browse() {
               }</h4>
               <h4>{show.location.name}</h4>
               <p>{show.location.address.streetAddress}</p>
-              <a href={show.offers[0].url}>link to venue</a>
+              {show.offers && show.offers[0] && show.offers[0].url ? <a href={show.offers[0].url}>link to venue</a> : null}
+              <br />
+              <button>wanna go - add to my dashboard</button>
               <hr />
             </li>
           )}
