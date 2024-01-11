ea02cc72c460c3601873dbca7eecaecfc6ce0966
Monica Barboza <145809292+MonBoza@users.noreply.github.com>
2024-01-11 16:35:04 -0600

    add styling Co-authored-by: Kim Robinson <kimmykokonut@gmail.com> Co-authored-by: Zuri Gallegos <Zurisadailopezmartinez@gmail.com>

--- src/components/Forum/CommentList.jsx
+++ src/components/Forum/CommentList.jsx
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import EditComment from "./EditComment";
 import { auth, db } from '../../firebase';
 import { collection, getDocs, doc, updateDoc, increment, setDoc, getDoc, orderBy, query } from "firebase/firestore";
+import { Container, Grid, Card, CardContent, CardHeader, Avatar, Button, IconButton, CardActions } from "@mui/material";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { red } from '@mui/material/colors';
+
 
 function CommentList() {
   const [comments, setComments] = useState([]);
@@ -35,7 +40,6 @@ function CommentList() {
 
     return () => clearInterval(intervalId);
   }, []);
-
   const handleCommentClick = (commentId) => {
     setSelectedCommentId(commentId);
   };
@@ -97,14 +101,14 @@ function CommentList() {
       const userId = auth.currentUser.uid;
       const userInteractionRef = doc(db, "userInteractions", `${userId}_${commentId}`);
       const userInteractionDoc = await getDoc(userInteractionRef);
-  
+
       if (!userInteractionDoc.exists()) {
         const commentDocRef = doc(db, 'post', commentId);
-  
-       
+
+
         await updateDoc(commentDocRef, { interested: increment(1) });
         setInterested((prevInterested) => prevInterested + 1);
-  
+
         await setDoc(userInteractionRef, {
           userId,
           commentId,
@@ -119,71 +123,79 @@ function CommentList() {
   };
 
   return (
-    <div>
-      <h3>Live Feed</h3>
-      {comments.length === 0 ? (
-        <p>No comments yet.</p>
-      ) : (
-
-          <div style={{
-            backgroundColor: "#f9f9f9",
-            marginBottom: "16px",
-            border: "1px solid #ccc",
-            padding: "8px",
-            cursor: "pointer",
-            position: "relative", }}>
-
-          {comments.map((comment) => (
-            <div
-              key={comment.id}
-              onClick={() => handleCommentClick(comment.id)}
-              style={{
-                // backgroundColor: "#60ce80",
-                backgroundColor: selectedCommentId === comment.id ? "#60ce80" : "transparent",
-                marginBottom: "16px",
-                border: "1px solid #ccc",
-                padding: "8px",
-                cursor: "pointer",
-                position: "relative",
-                backgroundColor: selectedCommentId === comment.id ? "#0d98ba " : "#fff",
-              }}
-            >
-              <p>{comment.text}</p>
-              <p>Posted By: {comment.userName}
-                  <img
-                    src={comment.profilePic || comment.profileImage}
-                    alt={`Profile of ${comment.userName}`}
-                    style={{ marginLeft: '8px', borderRadius: '50%', width: '32px', height: '32px' }}
-                  />
-                
-              </p>
-              <p>{comment.timeStamp.toDate().toLocaleString()}</p>
-              {selectedCommentId === comment.id && (
-                <div
-                  style={{
-                    position: "absolute",
-                    top: 0,
-                    right: 0,
-                    display: "flex",
-                    gap: "8px",
+    <div style={{ margin: '5%' }}>
+      <Container>
+        <h3>Live Feed</h3>
+        {comments.length === 0 ? (
+          <p>No comments yet.</p>
+        ) : (
+          <Grid container spacing={2}>
+            {comments.map((comment) => (
+              <Grid item xs={12} key={comment.id} >
+                <Card raised
+                  key={comment.id}
+                  onClick={() => handleCommentClick(comment.id)}
+                  sx={{
+                    width: "70%",
+                    variant: "outlined",
+                    display: 'inline-block',
+                    marginBottom: "16px",
+                    border: "1px solid #ccc",
+                    padding: "1px",
+                    cursor: "pointer",
+                    position: "relative",
+                    key: comment.id,
+                    backgroundColor: selectedCommentId === comment.id ? "#0d98ba " : "#fff",
                   }}
                 >
-                  <button onClick={() => handleGoing(comment.id)}>Going ({going})</button>
-                  <button onClick={() => handleInterested(comment.id)}>Interested ({interested})</button>
-                  <button onClick={() => handleNotGoing(comment.id)}>Not Going ({notGoing})</button>
-                </div>
-              )}
-            </div>
-          ))}
-        </div>
-      )}
-
+                  <CardHeader
+                    sx={{ pt: "10px", pb: "0px" }}
+                    avatar={
+                      <Avatar sx={{ bgcolor: red[500] }}aria-label="recipe">
+                        <img
+                          src={comment.profilePic || comment.profileImage}
+                          alt={`Profile of ${comment.userName}`}
+                          style={{ borderRadius: '50%', width: '32px', height: '32px' }}
+                        />
+                      </Avatar>
+                    }
+                    action={
+                      <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                      </IconButton>
+                    }
+                    title={comment.userName}
+                  />
+                  <CardContent sx={{ py: "0px" }}>
+                    <p>{comment.text}</p>
+                    <p>{comment.timeStamp.toDate().toLocaleString()}</p>
+                    <CardActions disableSpacing sx={{ py: "0px" }}>
+                      <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                      </IconButton>
+                      <div>
+                        {selectedCommentId === comment.id && (
+                          <div>
+                            <Button xs={12} onClick={() => handleGoing(comment.id)}>Going ({going})</Button>
+                            <Button xs={12} onClick={() => handleInterested(comment.id)}>Interested ({interested})</Button>
+                            <Button xs={12} onClick={() => handleNotGoing(comment.id)}>Not Going ({notGoing})</Button>
+                          </div>
+                        )}
+                      </div>
+                    </CardActions>
+                  </CardContent>
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Container>
       {selectedCommentId && (
         <EditComment
-        comment={comments.find((c) => c.id === selectedCommentId)}
-        setComments={setComments}
-        onClose={handleEditCommentClose}
-        userId={auth.currentUser?.uid}
+          comment={comments.find((c) => c.id === selectedCommentId)}
+          setComments={setComments}
+          onClose={handleEditCommentClose}
+          userId={auth.currentUser?.uid}
         />
       )}
     </div>
